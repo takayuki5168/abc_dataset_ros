9bd4ff05154414ac0915745b3d8ff0705c107748
Takayuki Murooka <takayuki5168@gmail.com>
2020-03-08 14:22:42 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+ABC Dataset ROS
+===============
+
+# Overview
+- Download CAD dataset whose format is STEP
+- Convert STEP files to STL files
+- Implement URDF files for each STL files
+- Render URDF files in Gazebo
---
+++ scripts/step_to_stl.py
@@ -0,0 +1,23 @@
+from PySide import QtCore
+import sys, os
+sys.path.append("/usr/lib/freecad/lib")
+import FreeCAD, Part, Mesh
+
+path = os.path.dirname(os.path.abspath(__file__)) + '/../models/step_test/'
+dirs = os.listdir(path)
+
+for i in range(len(dirs)):
+    dir_path = path + dirs[i]
+    for f in [dir_path + '/' + j for j in os.listdir(dir_path)]:
+        if '.step' in f:
+            file_path = f
+    #print(file_path)
+
+    shape = Part.Shape()
+    shape.read(file_path)
+
+    doc = App.newDocument('Doc')
+    pf = doc.addObject("Part::Feature","MyShape")
+    pf.Shape = shape
+    #print(file_path.split('.step')[0] + '.stl')
+    Mesh.export([pf], dir_path + '/model.stl')
