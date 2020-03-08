e21ec3cdad86fda557de688b7ab1eb0da327045e
Takayuki Murooka <takayuki5168@gmail.com>
2020-03-08 15:29:13 +0900

    some implementation

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 ABC Dataset ROS
 ===============
-
 ## Description
-ROS package to use ABC dataset<cite>[hoge][1]</cite> in ROS.
+ROS package to use ABC dataset (A Big CAD Model Dataset)<cite>[hoge][1]</cite> in ROS.
+
+![abc_gazebo](https://github.com/takayuki5168/abc_dataset_ros/blob/master/images/abc_gazebo.png)
 
 ## Demonstrations
 
@@ -17,15 +18,27 @@ $ sudo apt-get install p7zip-full
 ```
 
 ## Usage
-```
+### Preliminaries
+```bash
 $ cd ~/catkin_ws/src
 $ git clone https://github.com/takayuki5168/abc_dataset_ros
 $ cd abc_dataset_ros
 $ catkin bt
-$ ./scripts/download_step.sh                                       # Download CAD dataset whose format is STEP
-$ ./scripts/convert_step_to_stl.py                                 # Convert STEP files to STL files
-$ ./scripts/generate_sdf.sh                                        # Generate SDF files with STL files
-$ roslaunch gazebo_ros empty_world.launch world_name:=test.world   # Render SDF files in Gazebo
+$ ./scripts/download_step.sh         # Download CAD dataset whose format is STEP
+$ ./scripts/convert_step_to_stl.py   # Convert STEP files to STL files
+```
+
+### Render in Rviz
+```bash
+$ ./scripts/generate_udf.sh   # Generate UDF files with STL files
+$ roslaunch abc_dataset_ros rviz_demo.launch
+```
+
+### Render in Gazebo
+```bash
+$ ./scripts/generate_sdf.sh   # Generate SDF files with STL files
+$ roslaunch abc_dataset_ros gazebo_demo.launch
+#$ roslaunch gazebo_ros empty_world.launch world_name:=test.world
 ```
 
 [1] S. Koch, A. Matveev, Z. Jiang, F. Williams, A. Alexey, E. Burnaev, M. Alexa, D. Zorin, and Panozzo, Daniele. ABC: A Big CAD Model Dataset For Geometric Deep Learning. CVPR2019
---
+++ scripts/generate_sdf.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import sys, os
+
+path = os.path.dirname(os.path.abspath(__file__)) + '/../models/step_test/'
+dirs = os.listdir(path)
+
+for i in range(len(dirs)):
+    dir_path = path + dirs[i]
+
+    model_name = dirs[i]
+    print(model_name)
+
+    # model.config
+    config_str = "<?xml version=\"1.0\" ?>\n\
+<model>\n\
+  <name>" + model_name + "</name>\n\
+  <version>1.0</version>\n\
+  <sdf version=\"1.6\">model.sdf</sdf>\n\
+  <author>\n\
+    <name></name>\n\
+    <email></email>\n\
+  </author>\n\
+  <description></description>\n\
+</model>"
+
+    config_file_name = dir_path + '/model.config'
+    with open(config_file_name, mode='w') as f:
+        f.write(config_str)
+
+    # model.sdf
+    sdf_str = "<?xml version='1.0'?>\n\
+<sdf version=\"1.4\">\n\
+  <model name=\"" + model_name + "\">\n\
+    <pose>0 0 0.5 0 0 0</pose>\n\
+    <link name=\"link\">\n\
+      <inertial>\n\
+        <mass>1.0</mass>\n\
+        <inertia>\n\
+          <ixx>0.083</ixx>\n\
+          <ixy>0.0</ixy>\n\
+          <ixz>0.0</ixz>\n\
+          <iyy>0.083</iyy>\n\
+          <iyz>0.0</iyz>\n\
+          <izz>0.083</izz>\n\
+        </inertia>\n\
+      </inertial>\n\
+      <collision name=\"collision\">\n\
+        <geometry>\n\
+          <mesh>\n\
+            <uri>model://step_test/" + model_name + "/model.stl</uri>\n\
+            <scale>0.001 0.001 0.001</scale>\n\
+          </mesh>\n\
+        </geometry>\n\
+      </collision>\n\
+      <visual name=\"" + model_name + "\">\n\
+        <geometry>\n\
+          <mesh>\n\
+            <uri>model://step_test/" + model_name + "/model.stl</uri>\n\
+            <scale>0.001 0.001 0.001</scale>\n\
+          </mesh>\n\
+        </geometry>\n\
+      </visual>\n\
+    </link>\n\
+  </model>\n\
+</sdf>"
+
+    config_file_name = dir_path + '/model.sdf'
+    with open(config_file_name, mode='w') as f:
+        f.write(sdf_str)
